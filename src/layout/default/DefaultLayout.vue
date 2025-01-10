431d89b30d71dd37612b8689e020a12ec2c7ee4c
h0ngg0m <jaehongbyeon98@gmail.com>
2025-01-10 13:40:44 +0900

    :sparkles: 로그아웃

--- src/layout/default/DefaultLayout.vue
+++ src/layout/default/DefaultLayout.vue
@@ -2,7 +2,16 @@
   <v-app>
     <v-layout class="rounded rounded-md">
       <MenuItems />
-      <v-app-bar title="Application bar"></v-app-bar>
+      <v-app-bar>
+        <v-menu>
+          <template #activator="{ props }">
+            <v-btn icon="mdi-dots-vertical" variant="text" v-bind="props"></v-btn>
+          </template>
+          <v-list>
+            <v-list-item link prepend-icon="mdi-logout" title="Sign Out" @click="signOut" />
+          </v-list>
+        </v-menu>
+      </v-app-bar>
       <v-main class="d-flex align-center justify-center" style="min-height: 300px">
         <RouterView />
       </v-main>
@@ -13,4 +22,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import MenuItems from '@/layout/default/menu/MenuItems.vue'
+import { useAdminStore } from '@/store/adminStore.ts'
+import router from '@/router'
+
+const { clearAdminInfo } = useAdminStore()
+
+async function signOut(): Promise<void> {
+  clearAdminInfo()
+  await router.push('/auth/sign-in')
+}
 </script>
