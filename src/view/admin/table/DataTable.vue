d8feac4798b3a299659b0af853439bcae7c3cea6
h0ngg0m <jaehongbyeon98@gmail.com>
2025-01-15 08:52:36 +0900

    :lipstick: page count ui

--- src/view/admin/table/DataTable.vue
+++ src/view/admin/table/DataTable.vue
@@ -55,39 +55,46 @@
         </TableBody>
       </Table>
     </div>
-    <div class="flex items-center justify-end py-4 space-x-2">
-      <Button
-        variant="outline"
-        size="sm"
-        @click="() => table.setPageIndex(0)"
-        :disabled="!table.getCanPreviousPage()"
-      >
-        <ChevronsLeft />
-      </Button>
-      <Button
-        variant="outline"
-        size="sm"
-        @click="() => table.previousPage()"
-        :disabled="!table.getCanPreviousPage()"
-      >
-        <ChevronLeft />
-      </Button>
-      <Button
-        variant="outline"
-        size="sm"
-        @click="() => table.nextPage()"
-        :disabled="!table.getCanNextPage()"
-      >
-        <ChevronRight />
-      </Button>
-      <Button
-        variant="outline"
-        size="sm"
-        @click="() => table.setPageIndex(table.getPageCount() - 1)"
-        :disabled="!table.getCanNextPage()"
-      >
-        <ChevronsRight />
-      </Button>
+    <div class="flex flex-col">
+      <div class="flex justify-end space-x-2">
+        <span class="text-sm text-muted-foreground mt-1">
+          {{ `${pagination.pageIndex + 1} of ${pageCount}` }}
+        </span>
+      </div>
+      <div class="flex items-center justify-end py-4 space-x-2">
+        <Button
+          variant="outline"
+          size="sm"
+          @click="() => table.setPageIndex(0)"
+          :disabled="!table.getCanPreviousPage()"
+        >
+          <ChevronsLeft />
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          @click="() => table.previousPage()"
+          :disabled="!table.getCanPreviousPage()"
+        >
+          <ChevronLeft />
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          @click="() => table.nextPage()"
+          :disabled="!table.getCanNextPage()"
+        >
+          <ChevronRight />
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          @click="() => table.setPageIndex(table.getPageCount() - 1)"
+          :disabled="!table.getCanNextPage()"
+        >
+          <ChevronsRight />
+        </Button>
+      </div>
     </div>
   </div>
 </template>
