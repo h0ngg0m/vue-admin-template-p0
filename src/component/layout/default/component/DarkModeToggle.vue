8be4b110dd9fae46d35773dddf39ddb1c226b5ec
h0ngg0m <jaehongbyeon98@gmail.com>
2025-01-15 09:33:43 +0900

    :lipstick: dark mode toggle 버튼화

--- src/component/layout/default/component/DarkModeToggle.vue
+++ src/component/layout/default/component/DarkModeToggle.vue
@@ -1,21 +1,20 @@
 <template>
-  <Icon
-    v-if="mode === 'light'"
-    icon="radix-icons:moon"
-    @click="mode = 'dark'"
-    class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-  />
-  <Icon
-    v-else
-    icon="radix-icons:sun"
-    @click="mode = 'light'"
-    class="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-  />
+  <Button v-if="mode === 'light'" @click="mode = 'dark'" variant="ghost">
+    <Moon
+      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+    />
+  </Button>
+  <Button v-else @click="mode = 'light'" variant="ghost">
+    <Sun
+      class="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+    />
+  </Button>
 </template>
 
 <script setup lang="ts">
-import { Icon } from '@iconify/vue'
 import { useColorMode } from '@vueuse/core'
+import { Button } from '@/component/shadcn/ui/button'
+import { Moon, Sun } from 'lucide-vue-next'
 
 const mode = useColorMode()
 </script>
