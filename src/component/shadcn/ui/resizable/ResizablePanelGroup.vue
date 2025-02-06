102029f223e224dc9ee2f56f2e138c7341a3bb61
h0ngg0m <jaehongbyeon98@gmail.com>
2025-02-06 09:49:48 +0900

    :art: prettier

--- src/component/shadcn/ui/resizable/ResizablePanelGroup.vue
+++ src/component/shadcn/ui/resizable/ResizablePanelGroup.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import { cn } from '@/util/shadcn'
-import { SplitterGroup, type SplitterGroupEmits, type SplitterGroupProps, useForwardPropsEmits } from 'radix-vue'
+import {
+  SplitterGroup,
+  type SplitterGroupEmits,
+  type SplitterGroupProps,
+  useForwardPropsEmits,
+} from 'radix-vue'
 import { computed, type HTMLAttributes } from 'vue'
 
 const props = defineProps<SplitterGroupProps & { class?: HTMLAttributes['class'] }>()
@@ -15,7 +20,10 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
 </script>
 
 <template>
-  <SplitterGroup v-bind="forwarded" :class="cn('flex h-full w-full data-[panel-group-direction=vertical]:flex-col', props.class)">
+  <SplitterGroup
+    v-bind="forwarded"
+    :class="cn('flex h-full w-full data-[panel-group-direction=vertical]:flex-col', props.class)"
+  >
     <slot />
   </SplitterGroup>
 </template>
